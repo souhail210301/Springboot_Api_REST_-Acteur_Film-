0ec2eb2c47d8549d6c932de56daa28e0cdd4a8e7
Souhail Abdennebi <100965022+souhail210301@users.noreply.github.com>
2022-10-28 20:47:04 +0100

    Add files via upload

---
+++ acteurs/src/main/java/com/souhail/acteurs/entities/ActeurProjection.java
@@ -0,0 +1,5 @@
+package com.souhail.acteurs.entities;
+
+public interface ActeurProjection {
+
+}
---
+++ acteurs/src/main/java/com/souhail/acteurs/entities/Film.java
@@ -0,0 +1,27 @@
+package com.souhail.acteurs.entities;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Film {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idFilm;
+	private String nomFilm;
+	private String descriptionFilm;
+	@JsonIgnore
+	@OneToMany(mappedBy = "film")
+	private List<Acteur> acteurs;
+}
